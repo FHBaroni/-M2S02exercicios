d042a575cd9bf4b4ed36dc6231af7b8ac910328b
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-06-10 20:32:09 -0300

    M2S02Ex06

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("teste");
+        Console.Write(@"Seja bem vindo ao GeraEstoque 1.0
+----------------------------------------
+1  Cadastrar produto
+2  Consultar produto
+3  Modificar produto
+4  Excluir produto
+0  Sair
+
+Digite a opção: ");
     }
 }
