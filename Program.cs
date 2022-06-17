661bdaea9803042dd661667bd8ac0c6e099c2124
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-06-17 17:13:23 -0300

    M2S03Ex04

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using GeraEstoque.Screens;
 
 class Program
 {
-  static void Main(string[] args)
+  static void Main()
   {
     MenuScreen.Iniciar();
   }
--- Screens/CriarProdutoScreen.cs
+++ Screens/CriarProdutoScreen.cs
@@ -3,5 +3,33 @@
 
 public static class CriarProdutoScreen
 {
+    public static void Iniciar() {
+
+            var id = Guid.NewGuid();
+            Console.WriteLine(" Digite o nome do produto");
+            string nome = Console.ReadLine();
+            Console.WriteLine(" Digite a quantidade em estoque");
+            int quantidade = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" Digite o valor de compra");
+            decimal valorCompra = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(" Digite o valor de venda");
+            decimal valorVenda = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($@"
+  
+ Produto cadastrado com sucesso!
+
+ Nome: Produto {nome}
+ Qtd: {quantidade}
+ R$ Compra: R$ {valorCompra}
+ R$ Venda:  R$ {valorVenda}
+ ID: {id};
+
+ Pressione qualquer tecla para voltar ao menu principal");
+            Console.ReadKey();
+        MenuScreen.Iniciar();
+
+
+    }
+    
 	
 }
