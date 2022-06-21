ff6c49f27bf87a697f50db463896fbb732eb9970
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-06-21 20:38:49 -0300

    M2S03Ex06

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,22 @@
 namespace GeraEstoque.Models;
 
-    public class Produto
+public class Produto
 {
+    //construtor
+    public Produto(){}
+
+    // propriedades
+
+
+    public string id = Guid.NewGuid().ToString();
+    public string nome { get; set; }
+    public string quantidade { get; set; }
+    public int custo { get; set; }
+    public int venda { get; set; }
+
+    public override string ToString() => $" Produto: Id: {id} |  Nome: {nome} | Qtd: {quantidade} | R$ Compra: {custo} | R$ Venda: {venda}";
+
+
+    
 
 }
